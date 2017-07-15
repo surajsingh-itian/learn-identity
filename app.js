056e5942e577ff8504a61bb03944ee005f3d67c0
Moin Shaikh <shaikhmoin250@gmail.com>
2017-07-15 14:02:01 +0530

    add login routes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var identity = require('./routes/identity');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/identity', identity);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/identity.js
+++ routes/identity.js
@@ -3,9 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/login', function(req, res, next) {
-  res.json({res:'respond with a resource'} );
+ let username = req.body.email;
+  if (!username) {
+    res.json({
+      error: {
+        code: 10001,
+        message: 'username is missing',
+        description: 'username is required to log-in',
+        resolution: 'Kindly enter valid username.'
+      }
+    });
+    return;
+  }
+
+  // TODO: validate e-mail
+  // TODO: check e-mail exists
+
+  res.json({
+    message: 'Username has been sent to you on your registered email address.'
+  })
+
 });
-
-
 
 module.exports = router;
